32c974e1935a083ca226edc7ca5646029c07bf57
antonio <antoniorc7@gmail.com>
2024-02-23 11:19:21 +0100

    feat: healthy endpoint

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,15 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
+app.get('/healthy', (req, res) => {
+    res.status(200).json(
+        {
+            success: true,
+            message: 'Server is healthy'
+        }
+    );
+})
+
 app.listen(PORT, () => {
-    console.log('Server is running on port: $(PORT)')
+    console.log(`Server is running on port: $(PORT)`);
 })
